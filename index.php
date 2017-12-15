db096da1745455871410d405a2f42bc308586a62
chandrakn <pchandrakurniawan@gmail.com>
2017-12-15 07:31:10 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,3 @@
 <?php
-  $myServer = "192.168.102.34,1433";
-  $myUser = "chandrakn";
-  $myPass = "password";
-  $myDB = "Test3";
-
-  $conn = mssql_connect($myServer,$myUser,$myPass);
-  if (!$conn)
-  { 
-    die('Not connected : ' . mssql_get_last_message());
-  } 
-  $db_selected = mssql_select_db($myDB, $conn);
-  if (!$db_selected) 
-  {
-    die ('Can\'t use db : ' . mssql_get_last_message());
-  } 
+echo "tes"
 ?>
