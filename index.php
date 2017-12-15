b4936c380564100a77a0b0a9f812e7207b574a9c
chandrakn <pchandrakurniawan@gmail.com>
2017-12-15 07:39:01 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,10 @@
 <?php
-  $myServer = "192.168.102.34,1433";
-  $myUser = "chandrakn";
-  $myPass = "P@ssw0rd12345";
-  $myDB = "Test3";
-
-  $conn = mssql_connect($myServer,$myUser,$myPass);
-  if (!$conn)
-  { 
-    die('Not connected : ' . mssql_get_last_message());
-  } 
-  $db_selected = mssql_select_db($myDB, $conn);
-  if (!$db_selected) 
-  {
-    die ('Can\'t use db : ' . mssql_get_last_message());
-  } 
+	$serverName = "192.168.102.34,1433";
+	$connectionInfo = array("Database" => "Test3", "UID" => "chandrakn", "PWD" => "P@ssw0rd12345");
+	$conn = sqlsrv_connect($serverName, $connectionInfo);
+	if (!$conn) {
+		echo("Something went wrong while connecting to MSSQL");
+	} else {
+		echo "Connection Established";
+	}
 ?>
