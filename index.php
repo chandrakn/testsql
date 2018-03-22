08277ca5366bed7d2c8b85740456046bd3bafa3e
chandrakn <pchandrakurniawan@gmail.com>
2018-03-22 01:04:34 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 	$serverName = "192.168.102.34";
-	$connectionInfo = array("Database" => "Test5", "UID" => "chandra", "PWD" => "P@ssw0rd12345");
+	$connectionInfo = array("Database" => "db-chandra", "UID" => "chandra", "PWD" => "P@ssw0rd12345");
 	$conn = sqlsrv_connect($serverName, $connectionInfo);
 	if (!$conn) {
 		echo("Something went wrong while connecting to MSSQL");
