5de0a88bd59d7cb74398a989207d97b8bfa9a3c9
chandrakn <pchandrakurniawan@gmail.com>
2018-08-27 13:08:18 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
-	$serverName = "192.168.102.38";
-	$connectionInfo = array("Database" => "db-chandra", "UID" => "chandra", "PWD" => "P@ssw0rd12345");
+	$serverName = "192.168.102.34";
+	$connectionInfo = array("Database" => "Database01", "UID" => "user01", "PWD" => "IniAdalahPassword");
 	$conn = sqlsrv_connect($serverName, $connectionInfo);
 	if (!$conn) {
-		echo("Something went wrong while connecting to MSSQL");
+		echo("Connection Failed, please re-check your DB sredential");
 	} else {
 		echo "Connection Established";
 	}
