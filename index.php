c35448caf962e0e1f117d36846442824cde14a01
chandrakn <pchandrakurniawan@gmail.com>
2018-11-27 07:00:47 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 	$serverName = "192.168.102.45";
-	$connectionInfo = array("Database" => "Database01", "UID" => "user01", "PWD" => "IniAdalahPassword1!");
+	$connectionInfo = array("Database" => "Database01", "UID" => "user01", "PWD" => "Azure2018!");
+	//IniAdalahPassword1!
 	$conn = sqlsrv_connect($serverName, $connectionInfo);
 	if (!$conn) {
 		echo("Connection Failed, please re-check your DB sredential");
